71ecc67ed155fde93d58bcb32900decf560a205d
adeolaokunato-star <adeolaokunato@gmail.com>
2026-04-11 13:05:42 +0100

    Added new file

---
+++ exercise_five.py
@@ -0,0 +1,5 @@
+circle radius = 2
+diameter(2 * circle radius)
+circumference(2 * 3.14159 * circle radius)
+area = (3.14159 * circle radius ** circle radius)
+print('Area is", area)
---
+++ exercise_seven.py
@@ -0,0 +1,8 @@
+numone = 1024
+numtwo = 2
+if numone % 4 == 0:
+print('4 is a multiple', numone)
+if numtwo % 10 == 0
+print('10 is a multiple', numtwo)
+else:
+print('10 is not a multiple', numtwo)

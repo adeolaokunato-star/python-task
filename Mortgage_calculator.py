a3ea8616a4a0c8b9760561bb36ba32c08cd7e754
adeolaokunato-star <adeolaokunato@gmail.com>
2026-04-12 22:09:48 +0100

    Assinment

---
+++ Mortgage_calculator.py
@@ -0,0 +1,6 @@
+principal = int(input('Enter a number: '))
+air = int(input('Enter a annual interest rate: '))
+duration = int(input ('Enter duration in years: '))
+a = principal * air * duration
+print('Your monthly payment is: ', '$', a)
+
